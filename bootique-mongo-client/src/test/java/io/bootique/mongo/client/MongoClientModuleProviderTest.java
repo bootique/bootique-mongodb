b8d084c0830af403fe4994606d64b19c86626c4d
Andrus Adamchik <andrus@objectstyle.com>
2023-11-23 09:29:55 -0500

    stop using "test" prefix in test method names

--- bootique-mongo-client/src/test/java/io/bootique/mongo/client/MongoClientModuleProviderTest.java
+++ bootique-mongo-client/src/test/java/io/bootique/mongo/client/MongoClientModuleProviderTest.java
@@ -24,7 +24,7 @@ import org.junit.jupiter.api.Test;
 public class MongoClientModuleProviderTest {
 
     @Test
-    public void testAutoLoadable() {
+    public void autoLoadable() {
         BQModuleProviderChecker.testAutoLoadable(MongoClientModuleProvider.class);
     }
 }
